e5fa6725054382fb3d8713819c9a8c58de383a86
KacperKluska <kacperkluska130@gmail.com>
2021-10-25 20:06:25 +0200

    Setup commit

---
+++ app/src/main/java/com/pepega/mapoli/ui/map/view/MapFragment.java
@@ -0,0 +1,35 @@
+package com.pepega.mapoli.ui.map.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.pepega.mapoli.databinding.FragmentMapBinding;
+
+public class MapFragment extends Fragment {
+
+    private FragmentMapBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentMapBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
