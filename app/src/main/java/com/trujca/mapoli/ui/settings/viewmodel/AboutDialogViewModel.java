82ef6f4fac2f896afdc3d61136f8a8f182319d43
KacperKluska <kacperkluska130@gmail.com>
2021-11-23 18:32:53 +0100

    code clean up

--- app/src/main/java/com/trujca/mapoli/ui/settings/viewmodel/AboutDialogViewModel.java
+++ app/src/main/java/com/trujca/mapoli/ui/settings/viewmodel/AboutDialogViewModel.java
@@ -1,11 +1,7 @@
 package com.trujca.mapoli.ui.settings.viewmodel;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
 import javax.inject.Inject;
-
 import dagger.hilt.android.lifecycle.HiltViewModel;
 
 @HiltViewModel
@@ -14,6 +10,4 @@ public class AboutDialogViewModel extends ViewModel {
     @Inject
     public AboutDialogViewModel() {
     }
-
-    public LiveData<String> liveData = new MutableLiveData<>();
 }
