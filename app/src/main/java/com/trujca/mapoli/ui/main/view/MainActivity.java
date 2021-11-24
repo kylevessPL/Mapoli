e57e4b0c8f2cd3b5ed06ad0f4095fe94631a1e4e
Lubgost <Patryk_Gajda@outlook.com>
2021-11-24 10:52:32 +0100

    Added favourites list

--- app/src/main/java/com/trujca/mapoli/ui/main/view/MainActivity.java
+++ app/src/main/java/com/trujca/mapoli/ui/main/view/MainActivity.java
@@ -3,7 +3,10 @@ package com.trujca.mapoli.ui.main.view;
 import static java.util.Objects.requireNonNull;
 
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
+import androidx.appcompat.widget.PopupMenu;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
@@ -18,7 +21,7 @@ import com.trujca.mapoli.ui.base.BaseActivity;
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class MainActivity extends BaseActivity<ActivityMainBinding> {
+public class MainActivity extends BaseActivity<ActivityMainBinding>{
 
     private NavController navController;
     private NavigationView navView;
@@ -58,5 +61,23 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
         appBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_map).setOpenableLayout(drawerLayout).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    public void favouritesPopup(MenuItem item) {
+        View favouritesButton = findViewById(R.id.action_favourites);
+        PopupMenu popup = new PopupMenu(this, favouritesButton);
+        popup.getMenu().add("Weeia");                                                                   // TODO: Fill with binded data
+        popup.getMenu().add("Mordor");
+        popup.getMenu().add("Lodex");
+        popup.getMenu().add("Akwarium");
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                item.getTitle();                                                                        // TODO: Make action depending on items details (id + name)
+                return true;
+            }
+        });
+        popup.inflate(R.menu.favourites_list);
+        popup.show();
     }
 }
