324d159d256fd404a6125ad17891100a202f5ed1
Kacper Piasta <kacperpiasta@gmail.com>
2021-11-28 20:42:12 +0100

    fix user activity double appbar issue

--- app/src/main/java/com/trujca/mapoli/ui/user/view/UserActivity.java
+++ app/src/main/java/com/trujca/mapoli/ui/user/view/UserActivity.java
@@ -1,10 +1,26 @@
 package com.trujca.mapoli.ui.user.view;
 
+import static android.view.MotionEvent.ACTION_DOWN;
+import static java.util.Objects.requireNonNull;
+
+import android.graphics.Rect;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavGraph;
+import androidx.navigation.fragment.NavHostFragment;
+
 import com.trujca.mapoli.R;
 import com.trujca.mapoli.databinding.ActivityUserBinding;
 import com.trujca.mapoli.ui.base.BaseActivity;
 import com.trujca.mapoli.ui.user.viewmodel.UserViewModel;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class UserActivity extends BaseActivity<ActivityUserBinding, UserViewModel> {
 
     @Override
@@ -15,5 +31,44 @@ public class UserActivity extends BaseActivity<ActivityUserBinding, UserViewMode
     @Override
     protected int getLayoutRes() {
         return R.layout.activity_user;
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(final MotionEvent ev) {
+        View currentFocus = getCurrentFocus();
+        if (ev.getAction() != ACTION_DOWN || !(currentFocus instanceof EditText)) {
+            return super.dispatchTouchEvent(ev);
+        }
+        Rect outRect = new Rect();
+        currentFocus.getGlobalVisibleRect(outRect);
+        if (!outRect.contains((int) ev.getRawX(), (int) ev.getRawY())) {
+            currentFocus.clearFocus();
+            InputMethodManager imm = (InputMethodManager) getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(currentFocus.getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    protected void setup() {
+        setupActionBar();
+        setupNavController();
+    }
+
+    private void setupActionBar() {
+        setSupportActionBar(binding.toolbar);
+        setTitle(R.string.my_account);
+        requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setupNavController() {
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(binding.contentUser.navHostUser.getId());
+        NavGraph graph = requireNonNull(navHostFragment).getNavController().getGraph();
+        if (viewModel.getCurrentUser().getValue() == null) {
+            graph.setStartDestination(R.id.nav_login);
+        } else {
+            graph.setStartDestination(R.id.nav_account);
+        }
+        requireNonNull(navHostFragment).getNavController().setGraph(graph);
     }
 }
